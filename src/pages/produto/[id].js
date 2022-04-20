e06a76dcdb84182608ff6258d9fdc94ba389557b
unknown <leo.sn159@gmail.com>
2022-04-19 21:14:39 -0300

    Solução do CORS

--- src/pages/produto/[id].js
+++ src/pages/produto/[id].js
@@ -18,7 +18,7 @@ export default function Product() {
       fazGet(url);
 
     }
-  });
+  } , [router]);
 
   return (
     <div>
--- src/services/api.js
+++ src/services/api.js
@@ -1,20 +1,20 @@
 export const fazGet = async (url) => {
 
     const requestOptions = {
-        mode: 'no-cors',
         method: 'GET',
         headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, PATCH, DELETE, OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Origin, Access-Control-Allow-Methods, Access-Control-Allow-Headers',
         },
     };
     
     console.log("teste: "+url);
 
-    var request = new Request(url, requestOptions)
+    const request = new Request(url, requestOptions)
 
-    await fetch(request)
-    .then(function(data){
-        console.log(data);
-    });
+    const data = await fetch(request)
+    .then(res => res.json())
+    .catch(err => console.error(err))
+    console.log(data)
 }
